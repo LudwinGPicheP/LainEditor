557c64914a3ff7d80ec9f26f8c8f7b859efb241d
Ludwing Piche <ludgianpichepaz@gmail.com>
2023-12-12 23:02:52 -0600

    inicializacion del proyecto

---
+++ src/main/java/com/luna/laineditor/LainEditor.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.luna.laineditor;
+
+/**
+ *
+ * @author luna
+ */
+public class LainEditor {
+
+    
+}
